d7a7c599063fb2b850029ac561b59e0bd22d2a3d
akash <akashajay93953@gmail.com>
2023-04-01 08:29:37 +0000

    superoverleaguereact1

---
+++ index.js
@@ -0,0 +1,17 @@
+const element = (
+//   Write your code here.
+<div className = "Super-Over-League-Container">
+     <h1 className = "heading">Super Over League</h1>
+     <div className = "team-con">
+         <img src = "https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+         className = logo-img />
+         <img src = "https://assets.ccbp.in/frontend/react-js/csk-img.png"
+         className = logo-img />
+
+     </div>
+
+
+</div
+)
+
+ReactDOM.render(element, document.getElementById('root'));
